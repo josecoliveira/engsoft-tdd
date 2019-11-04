70a46895ac0cd8e899a221f5efbbf7f1a731990a
José Oliveira <josecarlosdeoliveirajr@gmail.com>
2019-11-04 10:20:26 -0300

    Step 4

--- src/Tests.java
+++ src/Tests.java
@@ -26,5 +26,11 @@ class Tests {
 		assertEquals(Money.franc(10), five.times(2));
 		assertEquals(Money.franc(15), five.times(3));
 	}
+	
+	@Test
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
 
 }
