7fd0de0de52d37e2f3060c3ba720ec1181b8e09f
José Oliveira <josecarlosdeoliveirajr@gmail.com>
2019-11-04 09:29:17 -0300

    Step 1

---
+++ src/Dollar.java
@@ -0,0 +1,6 @@
+
+class Dollar {
+	int amount = 10;
+	Dollar(int amount) {}
+	void times(int multiplier) {}
+}
---
+++ src/DollarTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DollarTest {
+
+	@Test
+	public void testMultiplication() {
+       Dollar five = new Dollar(5);
+       five.times(2);
+       assertEquals(10, five.amount);
+    }
+
+}
